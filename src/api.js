1a9e47ad7426f3293f18796778d1a8e63fc007e6
Joe Cooper <joecooper2991@gmail.com>
2020-06-18 12:51:04 +0100

    call open weather api

--- src/api.js
+++ src/api.js
@@ -1,3 +1,12 @@
 import axios from "axios";
 
-export const getWeather = () => {}
+export const getWeather = (city = "Manchester,uk") => {
+  return axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=10a7d5cb29b8e5a868ac5018958c7c74`
+    )
+    .then((result) => {
+      console.log(result.data);
+      return result.data;
+    });
+};

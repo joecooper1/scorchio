3599c35c5fcf25f870be0d80e45b46e4a2de5827
Joe Cooper <joecooper2991@gmail.com>
2020-06-19 17:11:33 +0100

    add styled components

--- src/Components/Foreground/infobox.js
+++ src/Components/Foreground/infobox.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { Info } from "../../styles/infobox.styles";
+import { Info, Temp, City } from "../../styles/infobox.styles";
 
 export default function InfoBox({ weather }) {
   return (
     <Info>
-      <h1>{weather.name}</h1>
-      <h2>{Math.round(weather.main.temp)}°</h2>
+      <Temp>{Math.round(weather.main.temp)}°</Temp>
+      <City>{weather.name}</City>
     </Info>
   );
 }

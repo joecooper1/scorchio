20dfd1da0050c960733fa750dfa932e84e88939f
Joe Cooper <joecooper2991@gmail.com>
2020-06-19 12:12:03 +0100

    get units in metric

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getWeather = (city = "Manchester,uk") => {
   return axios
     .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=10a7d5cb29b8e5a868ac5018958c7c74`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=10a7d5cb29b8e5a868ac5018958c7c74&units=metric`
     )
     .then((result) => {
       console.log(result.data);

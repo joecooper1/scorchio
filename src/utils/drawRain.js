7ac5bae084de3ee7a53cf979af274ee5dcf58482
Joe Cooper <joecooper2991@gmail.com>
2020-06-19 17:12:09 +0100

    create draw rain function

---
+++ src/utils/drawRain.js
@@ -0,0 +1,75 @@
+export default function drawRain(ctx, type, windSpeed, width, height) {
+  //Set empty array of raindrops
+  const rainDrops = [];
+
+  //Set number of raindrops to show on screen at once
+  //Light drizzle options set it to 20
+  let maxRainDrops = 10;
+  switch (type) {
+    case "light rain":
+    case "shower rain":
+    case "light intensity shower rain":
+    case "ragged shower rain":
+    case "heavy shower rain and drizzle":
+    case "heavy intensity drizzle rain":
+    case "heavy intensity drizzle":
+    case "thunderstorm with light rain":
+    case "thunderstorm with heavy drizzle":
+    case "light thunderstorm":
+      maxRainDrops = 50;
+      break;
+    case "moderate rain":
+    case "freezing rain":
+    case "thunderstorm with rain":
+    case "ragged thunderstorm":
+      maxRainDrops = 80;
+      break;
+    case "heavy intensity rain":
+    case "very heavy rain":
+    case "heavy intensity shower rain":
+    case "thunderstorm with heavy rain":
+    case "thunderstorm":
+    case "heavy thunderstorm":
+      maxRainDrops = 250;
+      break;
+    case "extreme rain":
+      maxRainDrops = 400;
+      break;
+  }
+
+  //Fill array with raindrops
+  for (let i = 0; i < maxRainDrops; i++) {
+    rainDrops.push({
+      x: Math.random() * width,
+      y: (height / maxRainDrops) * i,
+    });
+  }
+
+  //Set draw style
+  ctx.strokeStyle = "rgb(100, 100, 224)";
+  //   ctx.strokeStyle = "red";
+  ctx.lineWidth = 1;
+
+  //Function to calculate the rain positions
+  const makeItRain = () => {
+    ctx.clearRect(0, 0, width, height);
+    for (let i = 0; i < rainDrops.length; i++) {
+      const drop = rainDrops[i];
+      drop.y += 20;
+      drop.x += windSpeed / 5;
+      if (drop.y > height) {
+        drop.y = Math.random() * -30;
+        drop.x = Math.random() * (width + windSpeed * 4) - windSpeed * 4;
+      }
+      //Draw each drop
+      ctx.beginPath();
+      ctx.moveTo(drop.x, drop.y);
+      ctx.lineTo(drop.x + windSpeed / 5, drop.y + 10);
+      ctx.stroke();
+    }
+  };
+
+  setInterval(() => {
+    makeItRain();
+  }, 30);
+}

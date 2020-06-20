133ee824523dc740b9668d5c5d31d225d382b0f9
Joe Cooper <joecooper2991@gmail.com>
2020-06-20 09:49:08 +0100

    create sky component

---
+++ src/Components/Background/sky.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import calculateColor from "../../utils/calulateColor";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+export default function Sky({ weather, time }) {
+  const bgColor = calculateColor(weather, time);
+
+  return (
+    <div
+      style={{
+        backgroundColor: bgColor,
+        position: "absolute",
+        height: height,
+        width: width,
+      }}
+    ></div>
+  );
+}

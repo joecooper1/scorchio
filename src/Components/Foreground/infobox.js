c7e9166a38f65e0ca206dc23f84808e49b7b4e69
Joe Cooper <joecooper2991@gmail.com>
2020-06-21 17:31:09 +0100

    comment out image

--- src/Components/Foreground/infobox.js
+++ src/Components/Foreground/infobox.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-import { Info, Temp, City } from "../../styles/infobox.styles";
+import { Info, Temp, City, Image } from "../../styles/infobox.styles";
 
 export default function InfoBox({ weather }) {
   return (
     <Info>
       <Temp>{Math.round(weather.main.temp)}°</Temp>
+      {/* <Image
+        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+      ></Image> */}
       <City>{weather.name}</City>
     </Info>
   );

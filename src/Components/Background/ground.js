e3d2ba23dcbb1886c431ff442ef5c04ebc0a423e
Joe Cooper <joecooper2991@gmail.com>
2020-06-22 22:07:13 +0100

    add ground component

---
+++ src/Components/Background/ground.js
@@ -0,0 +1,39 @@
+import React, { useRef, useEffect, useState } from "react";
+
+import drawGround from "../../utils/drawGround";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+export default function Ground({ weather, time }) {
+  const canvasRef = useRef(null);
+  const [bgColor, setBgColor] = useState(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+
+      //Draw things here
+      drawGround(ctx, weather, time, height, width);
+    }
+  });
+
+  if (
+    (weather.weather[0].main === "Fog" ||
+      weather.weather[0].main === "Mist" ||
+      weather.weather[0].main === "Haze") &&
+    bgColor === null
+  ) {
+    setBgColor("rgba(255, 255, 255, 0.5)");
+  }
+
+  return (
+    <canvas
+      ref={canvasRef}
+      height={height}
+      width={width}
+      style={{ position: "absolute", backgroundColor: bgColor }}
+    ></canvas>
+  );
+}

88c95871b7d7b0035ff227cf2a90689ddf345a5b
Joe Cooper <joecooper2991@gmail.com>
2020-06-23 17:42:14 +0100

    pass dimensions as props

--- src/Components/Background/ground.js
+++ src/Components/Background/ground.js
@@ -2,10 +2,7 @@ import React, { useRef, useEffect, useState } from "react";
 
 import drawGround from "../../utils/drawGround";
 
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-export default function Ground({ weather, time }) {
+export default function Ground({ weather, time, width, height }) {
   const canvasRef = useRef(null);
   const [bgColor, setBgColor] = useState(null);
 

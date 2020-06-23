1b32d7ebfe2fbe441ec8998d8b7da125880f9c3a
Joe Cooper <joecooper2991@gmail.com>
2020-06-23 17:42:53 +0100

    inherit width and height

--- src/Components/Foreground/infobox.js
+++ src/Components/Foreground/infobox.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import { Info, Temp, City, Image } from "../../styles/infobox.styles";
 
-export default function InfoBox({ weather }) {
+export default function InfoBox({ weather, time, width, height }) {
   return (
-    <Info>
+    <Info width={width} height={height}>
       <Temp>{Math.round(weather.main.temp)}°</Temp>
       {/* <Image
         src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}

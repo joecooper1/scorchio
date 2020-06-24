5ec85245cd97153494cdeb647dbf6ab33685b548
Joe Cooper <joecooper2991@gmail.com>
2020-06-24 10:47:27 +0100

    add location options

--- src/Components/Options/optionsScreen.js
+++ src/Components/Options/optionsScreen.js
@@ -3,15 +3,29 @@ import React, { useState } from "react";
 import { OptionsBox } from "../../styles/options.styles";
 
 import WeatherOptions from "./weatherOptions";
+import LocationOptions from "./locationOptions";
 
 export default function OptionsScreen(props) {
   //Extract variables
-  const { weather, time, width, height, changeWeather } = props;
+  const {
+    weather,
+    time,
+    width,
+    height,
+    changeWeather,
+    changeCity,
+    changeCoords,
+  } = props;
 
   return (
     <OptionsBox width={width} height={height}>
       <h2>Options</h2>
-      <p>Input your location, or change the weather and time by hand.</p>
+      <p>Input your location, or change the weather and time manually.</p>
+      <LocationOptions
+        weather={weather}
+        changeCity={changeCity}
+        changeCoords={changeCoords}
+      />
       <WeatherOptions weather={weather} changeWeather={changeWeather} />
     </OptionsBox>
   );

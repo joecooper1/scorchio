4ce2e1df12a619949b99b42962b47cd23796ae49
Joe Cooper <joecooper2991@gmail.com>
2020-06-24 10:48:09 +0100

    add more styled components

--- src/styles/options.styles.js
+++ src/styles/options.styles.js
@@ -9,3 +9,29 @@ export const OptionsBox = styled.nav`
   max-width: 400px;
   height: ${(props) => `${props.height * 0.9}px`};
 `;
+
+export const WeatherOptionBox = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  height: 150px;
+`;
+
+export const LocationOptionBox = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  height: 150px;
+  margin-bottom: 50px;
+`;
+
+export const NumberInput = styled.input`
+  width: 50px;
+  border: 1px solid black;
+`;
+
+export const OrString = styled.p`
+  margin: 0px;
+`;

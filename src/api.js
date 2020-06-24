d7dace7c09e998bb62b9e1a99a2d6218eca428c5
Joe Cooper <joecooper2991@gmail.com>
2020-06-24 10:45:52 +0100

    allow for lat and lon inputs

--- src/api.js
+++ src/api.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 
-export const getWeather = (city = "Manchester,uk") => {
+export const getWeather = (city = "Manchester", country, lat, lon) => {
+  //Determine location
+  let location = "";
+  if (lat && lon) {
+    console.log("here");
+    location = `lat=${lat}&lon=${lon}`;
+  } else {
+    location = `q=${city},${country}`;
+  }
+  //Call api
   return axios
     .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=10a7d5cb29b8e5a868ac5018958c7c74&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?${location}&appid=10a7d5cb29b8e5a868ac5018958c7c74&units=metric`
     )
     .then((result) => {
       console.log(result.data);

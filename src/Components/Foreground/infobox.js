d65151a14955ce6538f564514bc23a7556596ee7
Joe Cooper <joecooper2991@gmail.com>
2020-06-24 16:10:25 +0100

    add time

--- src/Components/Foreground/infobox.js
+++ src/Components/Foreground/infobox.js
@@ -1,15 +1,38 @@
 import React from "react";
 
-import { Info, Temp, City, Image } from "../../styles/infobox.styles";
+import { Info, Temp, City, Image, Time } from "../../styles/infobox.styles";
 
-export default function InfoBox({ weather, time, width, height }) {
+import calculateColor from "../../utils/calulateColor";
+
+export default function InfoBox({
+  weather,
+  time,
+  width,
+  height,
+  hour,
+  minute,
+}) {
+  //Add 0 to numbers if single digits
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  //Get text color
+  const textColor = calculateColor(weather, time, "text");
+
   return (
-    <Info width={width} height={height}>
+    <Info width={width} height={height} color={textColor}>
       <Temp>{Math.round(weather.main.temp)}°</Temp>
       {/* <Image
         src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
       ></Image> */}
       <City>{weather.name}</City>
+      <Time>
+        Last updated {hour}:{minute}
+      </Time>
     </Info>
   );
 }

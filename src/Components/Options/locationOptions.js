dfc5119921143f1c71ce2d392116e6f9720d9944
Joe Cooper <joecooper2991@gmail.com>
2020-06-24 10:47:14 +0100

    add location options component

---
+++ src/Components/Options/locationOptions.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+import {
+  LocationOptionBox,
+  NumberInput,
+  OrString,
+} from "../../styles/options.styles";
+
+export default function LocationOptions(props) {
+  const [newCity, setNewCity] = useState(props.weather.name);
+  const [newCountry, setNewCountry] = useState(props.weather.sys.country);
+  const [newLatitude, setNewLatitude] = useState(props.weather.coord.lat);
+  const [newLongitude, setNewLongitude] = useState(props.weather.coord.lon);
+
+  function changeCity(event) {
+    setNewCity(event.target.value);
+  }
+
+  function changeLatitude(event) {
+    setNewLatitude(event.target.value);
+  }
+
+  function changeLongitude(event) {
+    setNewLongitude(event.target.value);
+  }
+
+  return (
+    <LocationOptionBox>
+      <label>
+        City:{" "}
+        <input type="string" onChange={changeCity} value={newCity}></input>
+      </label>
+      <button
+        onClick={() => {
+          props.changeCity(newCity);
+        }}
+      >
+        Go
+      </button>
+      <OrString>- or -</OrString>
+      <label>
+        Latitude:{" "}
+        <NumberInput
+          onChange={changeLatitude}
+          value={newLatitude}
+        ></NumberInput>
+      </label>
+      <label>
+        Longitude:{" "}
+        <NumberInput
+          onChange={changeLongitude}
+          value={newLongitude}
+        ></NumberInput>
+      </label>
+      <button
+        onClick={() => {
+          props.changeCoords(newLatitude, newLongitude);
+        }}
+      >
+        Go
+      </button>
+    </LocationOptionBox>
+  );
+}

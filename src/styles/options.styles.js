9523ec19f97044bfd0c51cb402ab1ccffee20454
Joe Cooper <joecooper2991@gmail.com>
2020-06-24 16:11:30 +0100

    add timeOptions style

--- src/styles/options.styles.js
+++ src/styles/options.styles.js
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export const OptionsBox = styled.nav`
   margin: 20px;
-  background-color: pink;
+  background-color: black;
   width: ${(props) => {
     return `${props.width - 400}px`;
   }};
   max-width: 400px;
   height: ${(props) => `${props.height * 0.9}px`};
+  padding: 10px;
+  color: white;
 `;
 
 export const WeatherOptionBox = styled.nav`
@@ -24,7 +26,16 @@ export const LocationOptionBox = styled.nav`
   align-items: flex-start;
   justify-content: space-between;
   height: 150px;
-  margin-bottom: 50px;
+  margin-bottom: 20px;
+`;
+
+export const TimeOptionBox = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  height: 40px;
+  margin-bottom: 20px;
 `;
 
 export const NumberInput = styled.input`

584e7b6f53164e7726fb95765d27be48b677bfc1
Joe Cooper <joecooper2991@gmail.com>
2020-06-24 16:12:03 +0100

    workout and return midnight

--- src/utils/useTime.js
+++ src/utils/useTime.js
@@ -1,9 +1,15 @@
-export default function useTime() {
+export default function useTime(customTime) {
   //Get time of day
-  const now = new Date();
+  let now;
+  if (customTime) now = new Date(customTime * 1000);
+  else now = new Date();
+  //Get hours and minutes
   const hour = now.getHours();
   const minute = now.getMinutes();
   //Add 3600 for each hour
   let time = Math.round(now.getTime() / 1000);
-  return time;
+  //Work out when midnight was
+  const midnight = time - (time % 60) - hour * 3600 - minute * 60;
+
+  return { time, hour, minute, midnight };
 }

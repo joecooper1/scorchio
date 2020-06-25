b85af39c053f685b0965d8339f53788366eb3ca3
Joe Cooper <joecooper2991@gmail.com>
2020-06-25 21:25:42 +0100

    add draw rainbow

--- src/Components/Background/clouds.js
+++ src/Components/Background/clouds.js
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 
 import drawClouds from "../../utils/drawClouds";
+import drawRainbow from "../../utils/drawRainbow";
 
 export default function Clouds({ weather, time, width, height }) {
   const canvasRef = useRef(null);
@@ -18,18 +19,19 @@ export default function Clouds({ weather, time, width, height }) {
         width
       );
 
+      const drawTheRainbow = drawRainbow(ctx, width, height);
+
       const id = setInterval(() => {
         moveClouds();
         drawTheClouds();
+        //If rain and not too many clouds, and daylight
+        if (weather.clouds.all < 50 && weather.weather[0].main === "Rain")
+          if (time < weather.sys.sunset && time > weather.sys.sunrise)
+            drawTheRainbow();
       }, 30);
-      //Draw things here
-      // if (weather.clouds.all > 0) {
-      //   const id = drawClouds(ctx, weather, time, height, width);
-      // }
+
       return () => clearInterval(id);
     }
-
-    // return () => clearInterval(id);
   });
 
   return (

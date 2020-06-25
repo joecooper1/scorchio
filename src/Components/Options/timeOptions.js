eac824ea0343be793c3d02c7926dbc0f92c62632
Joe Cooper <joecooper2991@gmail.com>
2020-06-25 21:26:41 +0100

    add and comment out time fast forward function

--- src/Components/Options/timeOptions.js
+++ src/Components/Options/timeOptions.js
@@ -6,6 +6,22 @@ export default function TimeOptions({ hour, minute, time, changeCustomTime }) {
   const [customHour, setCustomHour] = useState(hour);
   const [customMinute, setCustomMinute] = useState(minute);
 
+  //Fast forward time
+  // useEffect(() => {
+  //   console.log("once");
+  //   let hour = customHour;
+  //   let minute = customMinute;
+  //   setInterval(() => {
+  //     minute++;
+  //     if (minute === 60) {
+  //       hour++;
+  //       minute = 0;
+  //       if (hour === 24) hour = 0;
+  //     }
+  //     changeCustomTime(hour, minute);
+  //   }, 100);
+  // }, []);
+
   function changeTime(event) {
     setCustomHour(event.target.value.slice(0, 2));
     setCustomMinute(event.target.value.slice(3, 5));

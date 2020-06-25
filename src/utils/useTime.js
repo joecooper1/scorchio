37570be4ac06aa29f7018cc6be884524c31b9bee
Joe Cooper <joecooper2991@gmail.com>
2020-06-25 21:28:40 +0100

    add accounting for timezones

--- src/utils/useTime.js
+++ src/utils/useTime.js
@@ -1,17 +1,50 @@
 export default function useTime(customTime, weather) {
   //Set timezone if weather object exists
   const timezone = weather ? weather.timezone - 3600 : 0;
-  //Get time of day
+
+  //Initialise variabless
   let now;
-  if (customTime) now = new Date(customTime * 1000);
-  else now = new Date();
-  //Get hours and minutes
-  const hour = now.getHours() + timezone / 3600;
+  let hour;
+  let time;
+
+  if (customTime) {
+    //If time is set manually
+    //Make new date object based on customTime given
+    now = new Date(customTime * 1000);
+    //Get hour data
+    hour = now.getHours();
+    if (hour >= 24) {
+      hour -= 24;
+      // time -= 3600 * 24;
+    }
+    //Get time data
+    time = Math.round(now.getTime() / 1000);
+    //If on other side of world, reduce/increase time
+    if (timezone < -21600) time += 21600;
+    if (timezone > 21600) time -= 21600;
+  } else {
+    //If time comes from location
+    now = new Date();
+    //Add hours based on timezone
+    hour = now.getHours() + timezone / 3600;
+    //Get time data
+    time = Math.round(now.getTime() / 1000) + timezone;
+    //If next day
+    if (hour >= 24) {
+      hour -= 24;
+      time -= 3600 * 24;
+    }
+    //If on other side of world, reduce/increase time
+    if (timezone < -21600) time += 21600;
+    if (timezone > 21600) time -= 21600;
+  }
+
+  //Get minutes
   const minute = now.getMinutes();
-  //Add 3600 for each hour
-  let time = Math.round(now.getTime() / 1000) + timezone;
   //Work out when midnight was
   const midnight = time - (time % 60) - hour * 3600 - minute * 60;
+
+  console.log(time % 1593000000, hour, minute, midnight % 1593000000);
 
   return { time, hour, minute, midnight };
 }

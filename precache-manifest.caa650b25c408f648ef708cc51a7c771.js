b9eba7277dcda166878d5915e6a4453e99d11ecf
Joe Cooper <joecooper2991@gmail.com>
2020-06-26 14:30:43 +0100

    Updates

---
+++ precache-manifest.caa650b25c408f648ef708cc51a7c771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89a8e131218eee3eb371be81bf44e4b",
+    "url": "/scorchio/index.html"
+  },
+  {
+    "revision": "574c8dfbb8877e581b7d",
+    "url": "/scorchio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fdc3abb85aa946f9525d",
+    "url": "/scorchio/static/js/2.f3faf0b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/scorchio/static/js/2.f3faf0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574c8dfbb8877e581b7d",
+    "url": "/scorchio/static/js/main.49fa11f9.chunk.js"
+  },
+  {
+    "revision": "556e704a347f6f7d36a7",
+    "url": "/scorchio/static/js/runtime-main.468027af.js"
+  }
+]);
